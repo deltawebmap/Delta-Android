8e0cfb5ce797e75cf8a9dbb346dedf722108f598
Roman-Port <darkwings220@gmail.com>
2020-01-05 14:24:18 -0600

    Fixed hang bug if a structure image wasn't valid (for example, our app isn't updated)

--- app/src/main/java/com/romanport/deltawebmap/Framework/Views/MapStructures/ARKStructureImageCache.java
+++ app/src/main/java/com/romanport/deltawebmap/Framework/Views/MapStructures/ARKStructureImageCache.java
@@ -12,13 +12,24 @@ public class ARKStructureImageCache {
 
     private Context ctx;
     private HashMap<String, Bitmap> structureImageCache;
+    private Boolean isBitmapLoadComplete;
+    private Bitmap unknownBitmap;
 
     public ARKStructureImageCache(Context ctx) {
         this.ctx = ctx;
+        this.isBitmapLoadComplete = false;
         this.structureImageCache = new HashMap<>();
     }
 
     public void RunBackgroundThread() {
+        try {
+            //Quickly load the unknown bitmap
+            unknownBitmap = LoadBitmap("structure_unknown.png");
+        } catch (Exception ex) {
+            Log.d("ARKStructureImageCache", "Failed to load the unknown bitmap! We probably won't be able to load others.");
+        }
+
+        //Run background thread
         ARKStructureImageCacheLoader loader = new ARKStructureImageCacheLoader();
         loader.start();
     }
@@ -26,14 +37,18 @@ public class ARKStructureImageCache {
     public Bitmap GetBitmap(String name) {
         //If we haven't loaded it yet, but will shortly. Hang and wait
         //(this WILL block the UI thread, but it would anyways if we attempted to load this)
-        while(!structureImageCache.containsKey(name));
+        while(!structureImageCache.containsKey(name) && !isBitmapLoadComplete);
+
+        //If we still haven't found it, default to the unknown image. This might be used if we add new structure images and the user doesn't update the app
+        if(!structureImageCache.containsKey(name))
+            return unknownBitmap;
 
         //Return this from the cache
         return structureImageCache.get(name);
     }
 
     private Bitmap LoadBitmap(String name) throws Exception {
-        InputStream bitmap = ctx.getAssets().open("structures/"+name+".png");
+        InputStream bitmap = ctx.getAssets().open(name);
         Bitmap bp = BitmapFactory.decodeStream(bitmap);
         return bp;
     }
@@ -48,7 +63,7 @@ public class ARKStructureImageCache {
                 //Load each asset
                 for(String a : assets) {
                     String name = a.substring(0, a.length() - 4);
-                    Bitmap bp = LoadBitmap(name);
+                    Bitmap bp = LoadBitmap("structures/"+name+".png");
                     structureImageCache.put(name, bp);
                 }
 
